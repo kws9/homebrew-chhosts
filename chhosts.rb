8be389f4caa23b5ce4563f84044a5c71857542e2
Watabe <watabe.kohei@dena-travel.com>
2017-04-29 02:52:14 +0900

    update

--- chhosts.rb
+++ chhosts.rb
@@ -1,12 +1,12 @@
 REPOSITORY_URL="https://github.com/kws9/chhosts".freeze
-HOMEBREW_CHHOSTS_VERSION="0.0.1".freeze
+HOMEBREW_CHHOSTS_VERSION="0.0.2".freeze
 
 class Chhosts < Formula
   desc "Easy to switch hosts."
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/archive/#{HOMEBREW_CHHOSTS_VERSION}.tar.gz",
     :tag => HOMEBREW_CHHOSTS_VERSION.to_s
-  sha256 "19a66112127978d3bfd44d43f5a74d1b8cdb8ab3e436dc3b3ed2507012446cee"
+  sha256 "c54ebe1f87b897a09571991540cdb94a4fed1306e6c783776cb87481d07f857f"
   head "#{REPOSITORY_URL}.git", :branch => "master"
   def install
     prefix.install "bin"
